6853ec52e893cfe9c4d69087b47a313db3a2e7ec
patrick brisbin <pbrisbin@gmail.com>
2024-11-12 16:39:00 -0500

    Fix any type

--- src/repo-content.ts
+++ src/repo-content.ts
@@ -2,16 +2,22 @@ import * as github from "@actions/github";
 
 type ClientType = ReturnType<typeof github.getOctokit>;
 
+type RepoPath = {
+  content: string;
+  encoding: BufferEncoding;
+};
+
 export async function fetchRepoContent(
   client: ClientType,
   path: string,
 ): Promise<string> {
-  const response: any = await client.rest.repos.getContent({
+  const response = await client.rest.repos.getContent({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     ref: github.context.sha,
     path,
   });
 
-  return Buffer.from(response.data.content, response.data.encoding).toString();
+  const repoPath = response.data as RepoPath;
+  return Buffer.from(repoPath.content, repoPath.encoding).toString();
 }
