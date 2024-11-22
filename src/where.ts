a0b62bbfabe72e12a1017c708e6f2ec4304c04f2
patrick brisbin <pbrisbin@gmail.com>
2024-11-22 06:57:20 -0500

    Add where.diff support
    Closes #112.

--- src/where.ts
+++ src/where.ts
@@ -6,14 +6,17 @@ export type ConfigurationWhereClause = {
   path: {
     matches: string;
   };
-  additions_or_deletions?: {
-    contain: string[];
-  };
+  diff?: Diff;
   author?: {
     any: string[];
   };
   labels?: {
     any: string[];
+  };
+
+  // deprecated, still supported
+  additions_or_deletions?: {
+    contain: string[];
   };
 };
 
@@ -24,12 +27,17 @@ export function matches(
   const { changedFiles, author, labels } = changes;
   const matcher = new Minimatch(where.path.matches);
 
-  const hasFileMatch = changedFiles.some(
-    ({ filename, patch }) =>
-      matcher.match(filename) &&
-      (!where.additions_or_deletions ||
-        patchContains(patch, where.additions_or_deletions.contain)),
-  );
+  // Treat deprecated additions_or_deletions.contain as diff.contains, but note
+  // that if additions_or_deletions is present, diff is ignored.
+  const whereDiff = where.additions_or_deletions
+    ? { contains: where.additions_or_deletions.contain }
+    : where.diff;
+
+  const hasFileMatch = changedFiles.some(({ filename, patch: rawPatch }) => {
+    const patch = toPatch(rawPatch);
+    const matchedDiff = whereDiff ? matchesDiff(patch, whereDiff) : true;
+    return matcher.match(filename) && matchedDiff;
+  });
 
   const hasAuthorMatch =
     !where.author ||
@@ -41,5 +49,40 @@ export function matches(
   return hasFileMatch && hasAuthorMatch && hasLabelMatch;
 }
 
-const patchContains = (patch: string, needles: string[]) =>
-  needles.some((needle) => patch.includes(needle));
+type Patch = {
+  added: string[];
+  removed: string[];
+  lines: string[];
+};
+
+function toPatch(raw: string): Patch {
+  const lines = raw.split("\n");
+  const added = lines.filter((x) => x.match(/^\+ /));
+  const removed = lines.filter((x) => x.match(/^- /));
+  return { added, removed, lines };
+}
+
+type Diff = {
+  contains?: string[];
+  adds?: string[];
+  removes?: string[];
+};
+
+function matchesDiff(
+  { added, removed, lines }: Patch,
+  { contains, adds, removes }: Diff,
+): boolean {
+  const bs = [
+    contains && lines.length > 0
+      ? contains.some((x) => lines.some((l) => l.includes(x)))
+      : true,
+    adds && added.length > 0
+      ? adds.some((x) => added.some((l) => l.includes(x)))
+      : true,
+    removes && removed.length > 0
+      ? removes.some((x) => removed.some((l) => l.includes(x)))
+      : true,
+  ];
+
+  return bs.every((x) => x);
+}
